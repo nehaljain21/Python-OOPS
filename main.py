7e0cee30a5804e4dcb08e74563b7230756a070c6
nehaljain21 <nehalnj21@gmail.com>
2025-10-19 21:13:47 +0530

    Added deposit and withdraw method

--- main.py
+++ main.py
@@ -6,6 +6,22 @@ class BankAccount:
         self.__balance = balance
         BankAccount.total_balance += balance
 
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+            BankAccount.total_balance += amount
+            print(f"{amount} deposited. New balance: {self.__balance}")
+        else:
+            print("Deposit amount must be positive.")
+    
+    def withdraw(self, amount):
+        if 0 < amount <= self.__balance:
+            self.__balance -= amount
+            BankAccount.total_balance -= amount
+            print(f"{amount} withdrawn. New balance: {self.__balance}")
+        else:
+            print("Insufficient funds or invalid amount.")
+    
     @property
     def get_balance(self):
         return self.__balance
@@ -24,6 +40,9 @@ class BankAccount:
 
 acc1=BankAccount("Alice", 1000)
 acc2=BankAccount("Bob", 500)
-print(f"Alice's balance: {acc1.get_balance}")
-print(f"Bob's balance: {acc2.get_balance}")
-print(f"Total balance in bank: {BankAccount.get_total_balance()}")
+print(acc1.get_balance)
+amt=200
+acc1.deposit(amt)
+withdraw_amt=300
+acc1.withdraw(withdraw_amt)
+print(acc1.get_balance)
